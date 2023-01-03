b420447d584e7ecf747c7d0a25ce120886e32014
komugico <65069802+komugico@users.noreply.github.com>
2023-01-04 08:18:50 +0900

    Build react app and deploy

---
+++ docs/precache-manifest.223f95f07c4a3136f021fdf55696a6b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "16785b6dbaff2b81f27c",
+    "url": "/TsumeShogiTimer/static/css/main.e301e46f.chunk.css"
+  },
+  {
+    "revision": "16785b6dbaff2b81f27c",
+    "url": "/TsumeShogiTimer/static/js/main.16785b6d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/TsumeShogiTimer/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/TsumeShogiTimer/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "29aa16962cf7675afd06",
+    "url": "/TsumeShogiTimer/static/js/runtime~main.29aa1696.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TsumeShogiTimer/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9dd0e422f820b4aa8ea9bf6170a0401f",
+    "url": "/TsumeShogiTimer/index.html"
+  }
+];
